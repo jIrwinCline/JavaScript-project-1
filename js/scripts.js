e51454d05e2ec422d4b3a30eebb38f65db0238de
jirwincline <jakobinwins@gmail.com>
2019-06-04 00:10:01 -0700

    Completely redid program to imrove functionality and clear up ambiguous functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,141 @@
-var addCap = "";
-var L1 = "";
-var L2 = "";
-var sentenceOG = "";
+// var addCap = "";
+// var L1 = "";
+// var L2 = "";
+// var sentenceOG = "";
+//
+//
+// function mainFoo() {
+//
+// sentence = prompt("Please enter a sentence.");
+// sentenceOG = sentence;
+// function capFL(sentence) {
+// 	return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+// };
+//
+// L1 = sentence.charAt(0).toUpperCase();
+//
+//
+// var sentence2 = capFL(sentence);
+// var end = sentence.length - 1;
+//
+//
+// /* function capLL(sentence,end){
+//   return sentence.charAt(sentence.length).toUpperCase() + 		sentence.slice(sentence.length);
+// } */
+//
+// // function capLL(sentence,end){
+// // 	return sentence2.charAt(0).toUpperCase() + sentence2.slice(3);
+// //   }
+// //
+// // confirm("continue?");
+// //
+// // console.log(capLL(sentence));
+//
+// // var letter = function(end) {
+// //   return sentence2.charAt(end).toUpperCase();
+// // }
+//
+// // console.log(letter);
+//
+// function LL(end) {
+//   return sentence2.slice(0, end) + sentence2.charAt(end).toUpperCase()
+// };
+//
+// L2 = sentence2.charAt(end).toUpperCase();
+//
+// console.log(L1 + L2);
+// console.log(L2 + L1);
+//
+// var sentence3 = LL(end);
+// var twoCaps = L1 + L2;
+// var twoCapsR = L2 + L1;
+// console.log(sentence3);
+// var addCap = sentence + twoCapsR;
+// console.log(addCap);
+// return sentenceOG;
+// };
+// mainFoo();
+// function fourthFunc(sentenceOG) {
+//   var realLength = sentenceOG.length;
+//   var midStr = realLength / 2;
+//   midStr = Math.floor(midStr);
+//   console.log(midStr);
+//   console.log(realLength);
+//   var midChar = sentenceOG.charAt(midStr);
+//   console.log(midChar);
+//   console.log(midChar);
+// };
+// fourthFunc(sentenceOG);
+// console.log(addCap);
+////////////////////////////////////////////////////////////////////////////////////////
 
-
-function mainFoo() {
+//variables
+var sentence = "";
+var firstLetter = "";
+var lastLetter = "";
+var lettersCombined = "";
+var lettersCombinedR = "";
+var concSentence = "";
+var midCharacter = "";
 
 sentence = prompt("Please enter a sentence.");
-sentenceOG = sentence;
-function capFL(sentence) {
-	return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+
+
+function firstLastLetterfunc() {
+	function firstLetterfunc() {
+	 return sentence.charAt(0).toUpperCase();
+	};
+
+	firstLetter = firstLetterfunc();
+
+	function LastLetterfunc() {
+		return sentence.charAt(sentence.length - 1).toUpperCase();
+	};
+
+	lastLetter = LastLetterfunc();
+
+	lettersCombined = firstLetter + lastLetter;
+
 };
 
-L1 = sentence.charAt(0).toUpperCase();
+function lettersCombinedRfunc() {
+	lettersCombinedR = lastLetter + firstLetter;
+}
 
+function bothFuncs() {
+	firstLastLetterfunc();
+	lettersCombinedRfunc();
+	concSentence = sentence + lettersCombinedR;
 
-var sentence2 = capFL(sentence);
-var end = sentence.length - 1;
-
-
-/* function capLL(sentence,end){
-  return sentence.charAt(sentence.length).toUpperCase() + 		sentence.slice(sentence.length);
-} */
-
-// function capLL(sentence,end){
-// 	return sentence2.charAt(0).toUpperCase() + sentence2.slice(3);
-//   }
-//
-// confirm("continue?");
-//
-// console.log(capLL(sentence));
-
-// var letter = function(end) {
-//   return sentence2.charAt(end).toUpperCase();
-// }
-
-// console.log(letter);
-
-function LL(end) {
-  return sentence2.slice(0, end) + sentence2.charAt(end).toUpperCase()
 };
+function midCharFunc() {
+	var midChar = sentence.length / 2;
+	var midCharRound = Math.floor(midChar);
+	midCharacter = sentence.charAt(midCharRound);
+	concSentence = midCharacter + concSentence;
+}
 
-L2 = sentence2.charAt(end).toUpperCase();
+// firstLastLetterfunc();
+// lettersCombinedRfunc();
+bothFuncs();
+midCharFunc();
 
-console.log(L1 + L2);
-console.log(L2 + L1);
+console.log(sentence);
+console.log(firstLetter);
+console.log(lastLetter);
+console.log(lettersCombined);
+console.log(lettersCombinedR);
+console.log(concSentence);
+console.log(midCharacter);
+console.log(concSentence);
 
-var sentence3 = LL(end);
-var twoCaps = L1 + L2;
-var twoCapsR = L2 + L1;
-console.log(sentence3);
-var addCap = sentence + twoCapsR;
-console.log(addCap);
-return sentenceOG;
-};
-mainFoo();
-function fourthFunc(sentenceOG) {
-  var realLength = sentenceOG.length;
-  var midStr = realLength / 2;
-  midStr = Math.floor(midStr)
-  console.log(midStr);
-  console.log(realLength);
-  var midChar = sentenceOG.charAt(midStr);
-  console.log(midChar);
-  console.log(midChar);
-};
-fourthFunc(sentenceOG);
-console.log(addCap);
+////////////////////////////
+$(document).ready(function() {
+	$("h1").click(function() {
+		alert(sentence);
+	});
+	$("h2").click(function() {
+		alert(concSentence);
+	});
+});
